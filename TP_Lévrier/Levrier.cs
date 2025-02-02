984913dd19e89453e5b21f5e065427fba47b775c
Romain <Romain@DESKTOP-BGNDF0G>
2025-02-02 15:15:45 +0100

    Ajoutez des fichiers projet.

---
+++ TP_Lévrier/Levrier.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP_Lévrier
+{
+    internal class Levrier
+    {
+
+
+        public Random R { get; internal set; }
+        public Mutex mut { get; internal set; }
+
+        public int numero;
+
+        public ManualResetEvent start;
+
+        public ManualResetEvent end;
+
+
+        public void Run()
+        {
+
+            start.WaitOne();
+
+            R = new Random();
+
+            
+            for (int i = 0; i < 250; i++) 
+            {
+                mut.WaitOne();
+                int randomRange = R.Next(0, 10);
+                Console.ForegroundColor = (ConsoleColor)(numero + 1);
+                Console.WriteLine("Levrier " + numero + " : " + i);
+                mut.ReleaseMutex();
+                Thread.Sleep(randomRange);
+
+            }
+
+            end.Set();
+        }
+    }
+}
---
+++ TP_Lévrier/Program.cs
@@ -0,0 +1,77 @@
+﻿using System.Linq.Expressions;
+using TP_Lévrier;
+
+internal class Program
+{
+
+    
+
+    static void Main(string[] args)
+    {
+        List<int> podium = new List<int>();
+
+        
+
+        AutoResetEvent end = new AutoResetEvent(false);
+        
+        ManualResetEvent start = new ManualResetEvent(false);
+
+        Random r = new Random();
+
+        Console.Write("Nombre de lévrier entre 1 et 15 :");
+        int num = int.Parse(Console.ReadLine());
+
+        ManualResetEvent[] evnt = new ManualResetEvent[num];
+
+        Levrier levrier;
+        Mutex m = new Mutex(true);
+        
+        
+        
+
+        for (int i = 0; i < num; i++)
+        {
+           
+            levrier = new Levrier();
+            levrier.mut = m;
+            levrier.numero = i;
+            levrier.R = r;
+            levrier.end = new ManualResetEvent(false);
+            levrier.start = start;
+            evnt[i] = levrier.end;
+            Thread t = new Thread(levrier.Run);
+            t.Start();
+
+        }
+        start.Set();
+
+        int position;
+        int arrives = 0;
+
+        m.ReleaseMutex();
+
+
+        while (arrives < num)
+        {
+            position = WaitHandle.WaitAny(evnt);
+            m.WaitOne();
+            Console.WriteLine("Le n° " + position + " est arrivé !   <=======================================================================");
+            m.ReleaseMutex();
+            arrives++;
+            podium.Add(position);
+            evnt[position].Reset();
+        }
+
+        Console.ForegroundColor = ConsoleColor.White;
+        Console.WriteLine("     -----===== Tableau des arrivées =====-----");
+        foreach (int i in podium)
+        {
+            Console.WriteLine("Levrier " + i);
+        }
+
+        
+    }
+
+
+
+}
